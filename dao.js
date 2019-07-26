1467eff157e15ba314c9b8ce3e98d3c3f441b565
Glitch (sndvll-fcc-project-exercisetracker) <none>
2019-07-26 12:41:04 +0000

    🚍🌇 Checkpoint ./dao.js:5687688/495

--- dao.js
+++ dao.js
@@ -1,18 +1,26 @@
 const mongoose = require('mongoose')
 
+const errors = {
+  general: { error: 'Something wnt south...' },
+  userNotFound: { error: 'No user with ' }
+};
+
 const connect = () => {
   mongoose.connect(process.env.MONGO_URI +'/test?retryWrites=true&w=majority', { useNewUrlParser: true }).catch(err => console.log(err));
 };
 
-
 const userSchema = mongoose.Schema({
-  original_url: {
-    type: String,
-    required: true
-  },
-  route: Number
+  username: { type: String, required: true },
+  _id: { type: String, required: true }
 });
 
+const User = mongoose.model('excerciseUser', userSchema);
+
+const createUser = (username) => {
+  User.findOne({username}, (err, res) => {
+    
+  })
+};
 
 module.exports = {
   connect: connect
